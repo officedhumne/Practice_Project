f63ae5b0e846166a5a216dfab4a639f2075987e9
dhumne <office.dhumne@gmail.com>
2025-01-30 18:39:33 +0530

    withoutdynamotable

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ instance_type = var.instance_type
 key_name = var.key_name
 
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-lock"
+  hash_key     = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  }
