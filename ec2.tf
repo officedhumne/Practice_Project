1430b99dd40dffca1d03b135801390d25667f055
dhumne <office.dhumne@gmail.com>
2025-01-31 22:35:43 +0530

    cretedvpcnetwork

---
+++ ec2.tf
@@ -0,0 +1,89 @@
+#create vpc#
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name ="test server}"
+  }
+}
+
+#create subnet
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.test.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      name = "public_subnet"
+    }
+  
+}
+
+resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.test.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      name = "public_subnet"
+    }
+  
+}
+
+#internet gateway and attch to vpc
+
+resource "aws_internet_gateway" "test" {
+  vpc_id = aws_vpc.test.id
+}
+
+
+
+#create route table
+
+resource "aws_route_table" "test" {
+    vpc_id = aws_vpc.test.id
+    route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test.id
+  
+}
+}
+# subnet association
+
+resource "aws_route_table_association" "test"{
+route_table_id = aws_route_table.test.id
+subnet_id = aws_subnet.public.id
+
+} 
+  
+  #create security group
+
+  resource "aws_security_group" "allow_tls" {
+    name = "allow_tls"
+    vpc_id = aws_vpc.test.id
+    tags = {
+      name = "test_sg"
+    }
+ingress {
+    description = "tls from vpc"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ingress{
+  description = "tls from vpc"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+}
+  }
+  #create server
+
+  resource "aws_instance" "test" {
+    ami = "ami-00d65612a46d1c5f3"
+    instance_type = "t3.micro"
+    key_name = "first"
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  }
+
+
