9634861ae4950428717da2d8cad9bc5cef9b831c
dhumne <office.dhumne@gmail.com>
2025-02-28 21:09:06 +0530

    added location

--- jenkins/provider.tf
+++ jenkins/provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
- profile = "Rucha"        # Ensure this profile exists in your ~/.aws/credentials file
+ profile = "Rucha"  
+ shared_credentials_files  = ["C:/Users/rucha/.aws/credentials"]      # Ensure this profile exists in your ~/.aws/credentials file
   region  = "eu-north-1"
 }
